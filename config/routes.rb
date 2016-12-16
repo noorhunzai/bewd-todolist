7ac45a27a40b42527d3eb691fccb14c982936ecd
h8rry <harry@harrychen.net>
2016-12-16 12:03:13 +0800

    Add all specs and methods

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,11 @@ Rails.application.routes.draw do
 
   # Add routes below this line
 
-
-
-
+  get    'tasks'                    => 'tasks#index'
+  post   'tasks'                    => 'tasks#create'
+  delete 'tasks/:id'                => 'tasks#destroy'
+  put    '/tasks/:id/mark_complete' => 'tasks#mark_complete'
+  put    '/tasks/:id/mark_active'   => 'tasks#mark_active'
 
   # Add routes below above line
 
