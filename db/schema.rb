521d9fdfb0d10274c516c6ab33c07ac627ba004b
Harry <harrychenca@gmail.com>
2018-07-17 14:35:02 +0800

    Add other files

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20180717063321) do
+
+  create_table "tasks", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "content"
+    t.boolean "completed", default: false
+  end
 
 end
